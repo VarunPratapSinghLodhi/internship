1b74cd513a169a58ade7927b88c4e88e38f313a5
VarunPratapSinghLodhi <varunlodhi09@gmail.com>
2024-01-18 18:14:08 +0530

    This is my first commit

---
+++ day1.js/day1basics.js
@@ -0,0 +1,37 @@
+alert("hello from java script");
+// this is a example of semicolon 
+let price = 10;
+console.log(price);
+document.getElementById("this is a demo")
+.innerHTML="this is a line break example ";
+
+// creating a variable 
+var num1 =10;
+var num2;
+num2 =20;
+let sum =num1+num2;
+console.log(sum);
+
+// exaple of string 
+console.log("this is a string ");
+
+// operators in javascript 
+console.log(5*4+4);
+
+// js expression  
+// An expression is a combination of values, variables, and operators,
+//  which computes to a value.
+let num3=10;
+console.log(num3*4);
+
+// camel casing are of two type 
+let UpperCamelCase1 =10;
+let lowerCamelCase2 =20;
+
+// JavaScript Character Set
+// JavaScript uses the Unicode character set.
+
+// this is a single line  comment in JS 
+/*this is a multiline comment in JS */ 
+
+
